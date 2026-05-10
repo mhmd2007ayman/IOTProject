b2c1831c95363fdd611594efcc6c542faba2e227
mhmd2007ayman <mhmd2007ayman@gmail.com>
2026-05-10 04:06:47 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SensorController;
 
-Route::post('/sensor', [SensorController::class, 'store']);
-Route::get('/sensor',  [SensorController::class, 'latest']); 
+Route::post('/sensor',            [SensorController::class, 'store']);
+Route::get('/sensor',             [SensorController::class, 'latest']);
+Route::post('/sensor/clear-safe', [SensorController::class, 'clearSafe']);
+Route::delete('/sensor/{id}',     [SensorController::class, 'destroy']);
