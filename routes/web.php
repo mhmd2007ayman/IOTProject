f4c16b8cebd7426c046a0339694904b9ab9ebae0
mhmd2007ayman <mhmd2007ayman@gmail.com>
2026-05-10 03:33:13 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SensorController;
 
-Route::get('/', [SensorController::class, 'dashboard']);
+Route::get('/', [SensorController::class, 'dashboard']); 
