39d746db3485797db3e7c73ccad04bb36b585e87
mhmd2007ayman <mhmd2007ayman@gmail.com>
2026-05-10 03:33:43 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SensorController;
 
 Route::post('/sensor', [SensorController::class, 'store']);
-Route::get('/sensor',  [SensorController::class, 'latest']);
+Route::get('/sensor',  [SensorController::class, 'latest']); 
